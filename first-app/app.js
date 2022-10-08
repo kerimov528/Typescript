61250ffb8828e115b62f3d5480193a51723df599
kerimov528 <kerimov.anar528@gmail.com>
2022-10-08 12:58:09 -0700

    first commit

---
+++ first-app/app.js
@@ -0,0 +1,6 @@
+var result = document.getElementById("result");
+var op1 = sum(21, 10);
+function sum(num1, num2) {
+    return num2 + num1;
+}
+console.log(op1);
---
+++ first-app/app.ts
@@ -0,0 +1,9 @@
+const result = document.getElementById("result");
+let op1 = sum(21, 10);
+
+
+function sum(num1: number, num2: number) {
+    return num2 + num1
+}
+
+console.log(op1)
